e1666fb5010cf007e658c593fbdc5c68ff867869
Mirco Gonzalez <m1rc0.mg95@gmail.com>
2022-06-27 18:20:07 -0300

    refactor pass to props to ProfileCover component

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ProfileCover from "../../components/ProfileCover/ProfileCover";
-
+import { User } from "../../data";
 const Header = () => {
   return (
     <div className="container-fluid">
-      <ProfileCover />
+      <ProfileCover name={User.name} profession={User.profession} />
     </div>
   );
 };
