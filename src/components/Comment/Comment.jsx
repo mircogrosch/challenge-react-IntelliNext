3cf365f47c8732deb6bce1c84301ce8810d91763
Mirco Gonzalez <m1rc0.mg95@gmail.com>
2022-06-27 18:16:55 -0300

    feat add to consume data props and add media query

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -2,25 +2,23 @@ import React from "react";
 import { Avatar, Typography } from "@mui/material";
 import styles from "./Comment.module.css";
 import Likes from "../Likes/Likes";
-const Comment = ({ comment }) => {
+const Comment = ({ name, image, comment, numberLikes }) => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col col-md-1 col-lg-1 ">
-          <Avatar
-            alt="Avatar"
-            src={require("../../assets/img/profile.jpg")}
-            sx={{ width: 50, height: 50 }}
-          />
+        <div className={`col col-md-1 col-lg-1 ${styles.avatar}`}>
+          <Avatar alt="Avatar" src={image} sx={{ width: 50, height: 50 }} />
         </div>
         <div className="col col-md-11 col-lg-11 col-sm-11">
           <div className={styles.header}>
             <Typography variant="h1" color="textPrimary">
-              Mirco Gonzalez
+              {name}
             </Typography>
-            <Likes />
+            <div className={styles.likes}>
+              <Likes numberLikes={numberLikes} />
+            </div>
           </div>
-          <div className="comment">
+          <div className={styles.comment}>
             <Typography
               variant="h6"
               fontSize={15}
