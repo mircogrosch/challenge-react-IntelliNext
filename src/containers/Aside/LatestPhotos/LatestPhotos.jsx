4cdd041c72c9170884353530c0ed99df44440033
Mirco Gonzalez <m1rc0.mg95@gmail.com>
2022-06-27 18:19:24 -0300

    feat add tooltip to select pic and add hover

--- src/containers/Aside/LatestPhotos/LatestPhotos.jsx
+++ src/containers/Aside/LatestPhotos/LatestPhotos.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import { Photos } from "../../../data";
 import Wrapper from "../../../components/Wrapper/Wrapper";
 import { Typography } from "@mui/material";
+import styles from "./LatestPhotos.module.css";
+import Tooltip from "@mui/material/Tooltip";
+import Alert from "@mui/material/Alert";
 const LatestPhotos = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const handleClick = () => {
+    setShowAlert(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, "3000");
+  };
   return (
     <Wrapper>
       <Typography variant="h1" color={"textPrimary"} sx={{ marginBottom: 3 }}>
@@ -14,22 +24,26 @@ const LatestPhotos = () => {
         cols={3}
         gap={12}
         sx={{ width: 300, height: 300 }}
+        className={styles.imageList}
         rowHeight={50}
       >
         {Photos.map((image) => {
           return (
-            <ImageListItem key={image.id}>
-              <img
-                src={`${image.src}`}
-                alt={image.src}
-                loading="lazy"
-                width={50}
-                style={{ borderRadius: 10, width: 90, height: 90 }}
-              />
-            </ImageListItem>
+            <Tooltip title={"Buy Now"} onClick={() => handleClick()}>
+              <ImageListItem key={image.id} className={styles.img}>
+                <img
+                  src={`${image.src}`}
+                  alt={image.src}
+                  loading="lazy"
+                  width={50}
+                  style={{ borderRadius: 10, width: 90, height: 90 }}
+                />
+              </ImageListItem>
+            </Tooltip>
           );
         })}
       </ImageList>
+      {showAlert ? <Alert severity="success"> Buy success</Alert> : null}
     </Wrapper>
   );
 };
